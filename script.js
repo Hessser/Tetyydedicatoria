8e891b97e733141e88f031b7ec59c7cc9c070841
Hessser <hesserchacon12@gmail.com>
2024-11-21 15:03:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("frente").addEventListener("click", function () {
+  alert(
+    "Existen cosas bonitas en este mundo, casualidades inesperadas, paisajes inolvidables y luego estas tu, una bella cosntelacion en este mundo de oscuridad. TU, con tu sonrisa tan perfecta y con esa ternura que me enamora y que me provoca huracanes de felicidad. JIJI, quise intentar algo nuevo y espero y te guste este detalle, muaaaak, i love youu bb."
+  );
+});
+
+document.getElementById('beso').addEventListener('click', function() {
+    const besoImage = document.getElementById('besoImage');
+    besoImage.style.display = 'block'; // Muestra la imagen
+});
